83c80db76fe74cc36f7863024d0d7b1abace42a9
Ray Lee <ray.lee@lyrasis.org>
2020-01-03 22:13:52 -0800

    DRYD-744: Hide unused collectionobject forms.

--- src/plugins/recordTypes/collectionobject/forms/index.js
+++ src/plugins/recordTypes/collectionobject/forms/index.js
@@ -2,4 +2,13 @@ import defaultForm from './default';
 
 export default (configContext) => ({
   default: defaultForm(configContext),
+  inventory: {
+    disabled: true,
+  },
+  mini: {
+    disabled: true,
+  },
+  photo: {
+    disabled: true,
+  },
 });
